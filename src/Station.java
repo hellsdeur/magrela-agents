f620ea7f171021761585e7f0f3688e6ebb4654f3
Helder Matos <mtshelder@gmail.com>
2022-06-24 07:16:49 -0300

    Registering services on DFs

--- src/Station.java
+++ src/Station.java
@@ -1,7 +1,18 @@
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Station extends Agent {
 
@@ -13,35 +24,129 @@ public class Station extends Agent {
         int dockcount = Integer.parseInt(param[2].toString());
         float latitude = Float.parseFloat(param[3].toString());
         float longitude = Float.parseFloat(param[4].toString());
+        Queue<String> bikes = new LinkedList<String>();
 
-        addBehaviour(new OneShotBehaviour() {
+        // create a bike rental service
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Bike Service");
+        sd.setName("Rental");
+        registerService(sd);
+
+        // create a bike devolution service
+        sd = new ServiceDescription();
+        sd.setType("Bike Service");
+        sd.setName("Devolution");
+        registerService(sd);
+
+        // confirmation print (might delete later)
+        addBehaviour(new OneShotBehaviour(this) {
             @Override
             public void action() {
                 System.out.println("✓ Station " + getAID().getLocalName() + " created successfully.");
             }
         });
 
+        // request bikes from central
+        addBehaviour(new OneShotBehaviour(this) {
+            @Override
+            public void action() {
+                ACLMessage bikesRequest = new ACLMessage(ACLMessage.REQUEST);
+                bikesRequest.addReceiver(new AID("Central", AID.ISLOCALNAME));
+                bikesRequest.setOntology("Bike-Allocation");
+                bikesRequest.setContent(Integer.toString(dockcount));
+                myAgent.send(bikesRequest);
+
+                ACLMessage bikesReceived = myAgent.receive();
+                if (bikesReceived != null) {
+                    String content = bikesReceived.getContent();
+                    String[] arrStringBikes = content.split(" ");
+                    for (String stringBike: arrStringBikes) {
+                        bikes.add(stringBike);
+                    }
+                    System.out.println(bikes.size() + " bikes were received.");
+                }
+                else {
+                    block();
+                }
+            }
+        });
+
         addBehaviour(new CyclicBehaviour(this) {
             @Override
             public void action() {
-                ACLMessage bikeAllocMsg = myAgent.receive();
-                if (bikeAllocMsg != null) {
+                ACLMessage bikesReceived = myAgent.receive();
+                if (bikesReceived != null) {
+                    String content = bikesReceived.getContent();
+                    String[] arrStringBikes = content.split(" ");
+                    for (String stringBike: arrStringBikes) {
+                        bikes.add(stringBike);
+                    }
+                    System.out.println(bikes.size() + " bikes were received.");
+                }
+                else {
+                    block();
+                }
+            }
+        });
+
+        // listen for requests or devolution
+        addBehaviour(new CyclicBehaviour(this) {
+            @Override
+            public void action() {
+                ACLMessage receivedMessage = myAgent.receive();
+                if (receivedMessage != null) {
                     // create reply
-                    ACLMessage bikeReplyMsg = bikeAllocMsg.createReply();
-                    String content = bikeAllocMsg.getContent();
-                    if (content.equalsIgnoreCase("Bike")) {
-                        System.out.println("User " + bikeAllocMsg.getSender().getName() + " wants a bike.");
+                    ACLMessage reply = receivedMessage.createReply();
+                    String content = receivedMessage.getContent();
+                    if (content.equalsIgnoreCase("request")) {
+                        System.out.println("User " + receivedMessage.getSender().getName() + " wants a bike.");
                         System.out.println("Bike allocated!");
 
-                        bikeReplyMsg.setPerformative(ACLMessage.INFORM);
-                        bikeReplyMsg.setContent("Request accepted. Your bike ID is KKKKKK");
-                        myAgent.send(bikeReplyMsg);
+                        reply.setPerformative(ACLMessage.INFORM);
+                        reply.setContent(bikes.peek());
+                        bikes.remove();
+                        myAgent.send(reply);
+                    }
+                    else if (content.equalsIgnoreCase("devolution")) {
+                        System.out.println("User " + receivedMessage.getSender().getName() + " returned a bike.");
+                        System.out.println("Bike returned!");
+
+                        reply.setPerformative(ACLMessage.INFORM);
+                        reply.setContent("Devolution accepted.");
+                        myAgent.send(reply);
                     }
                     else {
+                        // TODO the problem is right here, this behaviour is catching messages from another one
+                        System.out.println(content);
                         block();
                     }
                 }
             }
         });
+    }
+
+    protected void registerService(ServiceDescription sd) {
+        // create a new entry at DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+
+        // add service
+        dfd.addServices(sd);
+
+        // register service at DF
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/User.java
+++ src/User.java
@@ -2,6 +2,8 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.core.behaviours.WakerBehaviour;
 import jade.lang.acl.ACLMessage;
 
 public class User extends Agent{
@@ -13,8 +15,10 @@ public class User extends Agent{
         String station = param[1].toString();
         float latitude = Float.parseFloat(param[2].toString());
         float longitude = Float.parseFloat(param[3].toString());
+        int delay = Integer.parseInt(param[4].toString());
+        String[] bike = new String[1];
 
-        addBehaviour(new OneShotBehaviour() {
+        addBehaviour(new OneShotBehaviour(this) {
             @Override
             public void action() {
                 System.out.println("✓ User " + getAID().getLocalName() + " created successfully.");
@@ -27,8 +31,8 @@ public class User extends Agent{
                 // bike allocation request message
                 ACLMessage bikeAllocMsg = new ACLMessage(ACLMessage.REQUEST);
                 bikeAllocMsg.addReceiver(new AID(station, AID.ISLOCALNAME));
-                bikeAllocMsg.setOntology("Bike-Request");
-                bikeAllocMsg.setContent("bike");
+                bikeAllocMsg.setOntology("BIKEREQUEST");
+                bikeAllocMsg.setContent("request");
                 myAgent.send(bikeAllocMsg);
             }
         });
@@ -39,11 +43,23 @@ public class User extends Agent{
                 ACLMessage bikeRecvMsg = myAgent.receive();
                 if (bikeRecvMsg != null) {
                     String content = bikeRecvMsg.getContent();
+                    bike[0] = content;
                     System.out.println("--->" + bikeRecvMsg.getSender().getName() + ": " + content);
                 }
                 else {
                     block();
                 }
+            }
+        });
+
+        addBehaviour(new WakerBehaviour(this, delay) {
+            @Override
+            protected void onWake() {
+                ACLMessage bikeDevolutionMessage = new ACLMessage(ACLMessage.REQUEST);
+                bikeDevolutionMessage.addReceiver(new AID(station, AID.ISLOCALNAME));
+                bikeDevolutionMessage.setOntology("Bike-Devolution");
+                bikeDevolutionMessage.setContent("devolution");
+                myAgent.send(bikeDevolutionMessage);
             }
         });
     }
